7828f7765281cfc3c3acb9b876cd72ac9494ba8c
youjin kim <gracecancho@gmail.com>
2023-05-15 16:06:07 +0900

    fetch api

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,25 +1,28 @@
 import { NextPage } from "next";
 import BlogCard from "../components/BlogCard";
+import { useEffect, useState } from "react";
 
 interface Props {}
 
 const Blogs: NextPage<Props> = () => {
+  const [posts, setPosts] = useState<
+    { title: string; slug: string; meta: string }[]
+  >([]);
+
+  const fetchPosts = async () => {
+    const res = await fetch("/api/posts").then((data) => data.json());
+    setPosts(res.postInfo);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <div className="max-w-3xl mx-auto p-5 space-y-5">
-      <BlogCard
-        title="This is my blog"
-        desc="consectetur adipisicing elit. Voluptatum, optio deserunt. Nihil
-        consequatur doloremque alias reiciendis, ut ratione fugiat distinctio
-        aliquam dolor dignissimos. Reprehenderit officia aliquid dolorum cum
-        asperiores libero."
-      />
-      <BlogCard
-        title="This is my blog"
-        desc="consectetur adipisicing elit. Voluptatum, optio deserunt. Nihil
-        consequatur doloremque alias reiciendis, ut ratione fugiat distinctio
-        aliquam dolor dignissimos. Reprehenderit officia aliquid dolorum cum
-        asperiores libero."
-      />
+      {posts.map((post, index) => (
+        <BlogCard key={index} title={post.title} desc={post.meta} />
+      ))}
     </div>
   );
 };
