324e46d356d29f424a0b5205b6d3d8a3f07a539d
youjin kim <gracecancho@gmail.com>
2023-05-15 16:13:05 +0900

    getStaticProps

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,23 +1,20 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import BlogCard from "../components/BlogCard";
 import { useEffect, useState } from "react";
 
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("http://localhost:3000/api/posts").then((data) =>
+    data.json()
+  );
+
+  return {
+    props: { posts: res.postInfo },
+  };
+};
+
 interface Props {}
 
-const Blogs: NextPage<Props> = () => {
-  const [posts, setPosts] = useState<
-    { title: string; slug: string; meta: string }[]
-  >([]);
-
-  const fetchPosts = async () => {
-    const res = await fetch("/api/posts").then((data) => data.json());
-    setPosts(res.postInfo);
-  };
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
+const Blogs: NextPage<Props> = ({ posts }) => {
   return (
     <div className="max-w-3xl mx-auto p-5 space-y-5">
       {posts.map((post, index) => (
