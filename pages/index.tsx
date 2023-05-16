8a96d8610342a8a1799ccc3b59b64567cacd00d6
youjin kim <gracecancho@gmail.com>
2023-05-16 09:35:41 +0900

    add the links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Welcome My Dev Blog</h1>
+      <Link href='/blogs'>
+        <p>Go to Blog</p>
+      </Link>
+      <Link href='/api/posts'>
+        <p>See the api-posts</p>
+      </Link>
+    </>
+  )
 };
 
 export default Home;
